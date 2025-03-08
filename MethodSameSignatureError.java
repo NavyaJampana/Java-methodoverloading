f4257b0afd9f42c1a483c053ffeebbbe34ffb670
NavyaJampana <jnavya2024@gmail.com>
2025-03-08 17:34:56 +0530

    Add files via upload

---
+++ MethodSameSignatureError.java
@@ -0,0 +1,13 @@
+package MethodOverloading;
+
+class MethodSameSignatureError {
+    void display(int a) {
+        System.out.println("First Method: " + a);
+    }
+
+    public static void main(String[] args) {
+        MethodSameSignatureError obj = new MethodSameSignatureError();
+        obj.display(10); 
+    }
+}
+
