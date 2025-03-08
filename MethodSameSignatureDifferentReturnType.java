f4869632c600e4ddb5c80ea441926939b6fea4c8
NavyaJampana <jnavya2024@gmail.com>
2025-03-08 17:34:38 +0530

    Add files via upload

---
+++ MethodSameSignatureDifferentReturnType.java
@@ -0,0 +1,11 @@
+package MethodOverloading;
+
+class MethodSameSignatureDifferentReturnType {
+    int display(int a) {
+        return a + 10;
+    }
+    public static void main(String[] args) {
+        MethodSameSignatureDifferentReturnType obj = new MethodSameSignatureDifferentReturnType();
+        System.out.println(obj.display(10)); 
+    }
+}
